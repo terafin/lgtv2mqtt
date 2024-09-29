add1a0f90c5aba9c611b02f801f2d49d83b23bfd
terafin <jwood@me.com>
2024-09-29 08:35:13 -0700

    Update index.js

--- index.js
+++ index.js
@@ -163,12 +163,12 @@ lgtv.on('connect', () => {
     lgtv.subscribe('ssap://audio/getVolume', (err, res) => {
         logging.info('audio/getVolume', err, res)
         if (!_.isNil(res) && !_.isNil(res.changed)) {
-            if (res.changed.indexOf('volume') !== -1) {
+//            if (res.changed.indexOf('volume') !== -1) {
                 mqtt.publish(topic_prefix + '/status/volume', String(res.volume), mqttOptions)
-            }
-            if (res.changed.indexOf('muted') !== -1) {
+//            }
+//            if (res.changed.indexOf('muted') !== -1) {
                 mqtt.publish(topic_prefix + '/status/mute', res.muted ? '1' : '0', mqttOptions)
-            }
+//            }
         }
         if (!_.isNil(res) && !_.isNil(res.volumeStatus)) {
             if (res.volumeStatus.indexOf('volume') !== -1) {
